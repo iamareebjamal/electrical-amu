6dbdaf668330c25b25cc435c75bf2c6385364b7c
divs4debu <divs4debu@gmail.com>
2016-02-27 13:39:51 +0530

    Minor changes 2

--- app/src/main/java/amu/electrical/deptt/DetailActivity.java
+++ app/src/main/java/amu/electrical/deptt/DetailActivity.java
@@ -19,6 +19,7 @@ package amu.electrical.deptt;
 import amu.electrical.deptt.utils.Colors;
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
@@ -34,6 +35,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.bumptech.glide.Glide;
 
@@ -53,6 +55,8 @@ public class DetailActivity extends AppCompatActivity implements AppBarLayout.On
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
         ab.setHomeButtonEnabled(true);
+
+
 
         collapsingToolbar = ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar));
         collapsingToolbar.setTitle("Department");
